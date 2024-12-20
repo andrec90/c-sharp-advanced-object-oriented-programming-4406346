86bf7a6bb6fc39f87f7bf3cd49a5a43483fbc7a0
andrec90 <79743848+andrec90@users.noreply.github.com>
2024-12-20 17:46:21 +0000

    First exercise

--- Start/Ch1/DefineClasses/Program.cs
+++ Start/Ch1/DefineClasses/Program.cs
@@ -2,6 +2,10 @@
 // Defining and instantiating classes
 
 // TODO: Create some new Rectangle objects with dimensions
+Rectangle rect1 = new Rectangle(10, 20);
+Rectangle rect2 = new Rectangle(30);
 
+Console.WriteLine(rect1.Area().ToString());
+Console.WriteLine(rect2.Perimeter().ToString());
 
 // TODO: Change the values of width and height
--- Start/Ch1/DefineClasses/shapes.cs
+++ Start/Ch1/DefineClasses/shapes.cs
@@ -2,15 +2,36 @@
 // Defining and instantiating classes
 
 // TODO: Define a class using the "class" keyword
-
+class Rectangle
+{
     // TODO: The constructor accepts parameters used to create the object
-
+    public Rectangle(int w, int h)
+    {
+        this.width = w;
+        this.height = h;
+    }
 
     // TODO: For convenience, we can have a constructor that takes one value
     // for squares that have the same side size
+    public Rectangle(int s)
+    {
+        this.width = s;
+        this.height = s;
+    }
 
 
     // TODO: Classes can define methods that return values
+    public Double Area()
+    {
+        return width*height;
+    }
 
+    public Double Perimeter()
+    {
+        return 2*width + 2*height;
+    }
 
     // TODO: member variables hold data
+    public int width;
+    public int height;
+}
